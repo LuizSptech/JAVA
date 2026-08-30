3b0c1396ecd36ade71895d3ae33548d6e1d2c503
LuizSptech <luiz.correia@sptech.school>
2026-08-30 01:42:11 -0300

    V1 logs

--- entrega-executavel/src/main/java/school/sptech/Main.java
+++ entrega-executavel/src/main/java/school/sptech/Main.java
@@ -1,18 +1,36 @@
 package school.sptech;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Aviso teste = new Aviso();
+        Logs teste = new Logs();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insira True or False");
+        Boolean lets = scanner.nextBoolean();
+        Thread jar = new Thread(() -> {
 
 
 
+       teste.Login(lets);
+            try {
+                Thread.sleep(8000);
+            } catch (InterruptedException e){
+                System.out.println("Erro");
+            }
 
-        teste.Login("Luiz","1234","12345");
-        teste.Login(null,null,null);
+
+            teste.capturaDados(lets);
+            try {
+                Thread.sleep(9000);
+            } catch (InterruptedException e){
+                System.out.println("Erro");
+            }
+
+            teste.tratamento(lets);
 
 
-        teste.capturaDados(false);
-        teste.capturaDados(true);
 
+        });
+        jar.start();
     }
 }
