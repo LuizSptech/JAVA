24fb180f4a80938f56bec526d0da199155848523
LuizSptech <luiz.correia@sptech.school>
2026-09-13 01:39:40 -0300

    progresso

--- introducao-encapsulamento-main/src/main/java/school/sptech/ex03/Data.java
+++ introducao-encapsulamento-main/src/main/java/school/sptech/ex03/Data.java
@@ -20,21 +20,111 @@ public class Data {
 
 
 
-    public void definitData(Integer dia, Integer mes, Integer ano){
+    public void definirData(Integer dia, Integer mes, Integer ano){
         this.dia = dia;
         this.mes = mes;
         this.ano = ano;
-        if (ano == null){
+        if (dia == null || mes == null || ano == null){
             return;
         }
         if ( mes < 1 || mes > 12 ){
             return;
         }
-        if (dia < 1 || dia > 31){
+        if (dia > 31 || dia < 1){
             return;
         }
+    }
 
-        
+    public String formatarData(){
+        String mensagem;
+        int a;
+        a = String.valueOf(ano).length();
+        if ((dia < 10 || mes < 10) && a == 1){
+            mensagem = "0" + dia + "/" + "0"+ mes + "/" + "0" + "0" + "0" +ano;
+            return mensagem;
+        }
+        if (dia < 10 || mes < 10){
+            mensagem = "0" + dia + "/" + "0"+ mes + "/" + ano;
+            return mensagem;
+        }
 
+        if (a == 2){
+            mensagem = dia + "/" + mes + "/" + "0" + "0" +ano;
+            return mensagem;
+        }
+        if (a == 3){
+            mensagem = dia + "/" + mes + "/" + "0" + ano;
+            return mensagem;
+        }
+
+        mensagem = dia + "/" + mes + "/" + ano;
+        return mensagem;
+    }
+
+
+
+    public Integer compararDatas(Integer dia, Integer mes, Integer ano){
+        if (dia == null || mes == null|| ano== null){
+            return null;
+        }
+
+      /*  Integer diaA = getDia();
+        Integer mesA = getMes();
+        Integer anoA = getAno();
+        Integer[] obj = {diaA, mesA, anoA};
+        Integer[] param = {dia,mes,ano};
+        if ((obj[2] > param[2]) || obj[1] > param[1]){
+            return -1;
+        }
+        if ((obj[2] < param[2]) || obj[1] < param[1]){
+            return 1;
+        }*/
+        /*
+        for (int i = 0; i < data1.length; i++) {
+            objeto += data1[i];
+            informada += data2[i];
+        }
+
+        if (informada > objeto){
+            return 1;
+        }
+        if (informada < objeto){
+            return -1;
+        }*/
+       /* String objetc = dia.toString() + mes.toString() + ano.toString() + "";
+        String info = getAno().toString() + getMes().toString() + getAno().toString() + "";
+        Integer objeto = Integer.parseInt(objetc);
+        Integer informado = Integer.parseInt(info);
+        if (informado > objeto){
+            return 1;
+        }
+        if (informado < objeto){
+            return -1;
+        }*/
+        Integer objeto = 0;
+        Integer informada = 0;
+        Integer retorno = 0;
+        Integer diaA = getDia();
+        Integer mesA = getMes();
+        Integer anoA = getAno();
+        Integer[] obj = {diaA, mesA, anoA};
+        Integer[] param = {dia,mes,ano};
+        for (int i = 0; i < obj.length; i++) {
+        if (obj[i] < param[i]){
+            retorno = 1;
+        }
+        if (obj[i] > param[i]){
+            retorno = -1;
+        }
+            objeto += obj[i];
+            informada += param[i];
+
+        }
+            if (objeto == informada){
+                return 0;
+            }
+
+
+        return retorno;
     }
 }
