90b81f8941bed2e85dffb74a075588c3ab3c0da6
LuizSptech <luiz.correia@sptech.school>
2026-09-16 14:23:01 -0300

    salvando progresso

--- exercicio-relacionamento-main/src/main/java/school/sptech/ex01/Carrinho.java
+++ exercicio-relacionamento-main/src/main/java/school/sptech/ex01/Carrinho.java
@@ -7,9 +7,64 @@ public class Carrinho {
     private String cliente;
     private List<Produto> produtos;
 
-    public Integer getQuantidade(){
-        
+    public Integer getQuantidade() {
+        return produtos.size();
+    }
+
+    public void adicionar(Produto p) {
+        produtos.add(p);
+    }
+
+    public Boolean existsPorNome(String nome) {
+        for (Produto produto : this.produtos) {
+            if (produto.getNome().equalsIgnoreCase(nome)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public Integer getQuantidadePorCategoria(String nome) {
+        Integer contador = 0;
+        for (Produto produto : produtos) {
+            if (produto.getCategoria().equalsIgnoreCase(nome)) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public void limpar() {
+        produtos.removeAll(produtos);
+    }
+
+    public void removerPorNome(String nome) {
+        if (nome == null) {
+            return;
+        }
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getNome().equalsIgnoreCase(nome)) {
+                produtos.remove(i);
+            }
+        }
+    }
+
+    public Produto getPorNome(String nome) {
+        for (int i = 0; i < produtos.size(); i++) {
+            if (produtos.get(i).getNome().equalsIgnoreCase(nome)) {
+                return produtos.get(i);
+            }
+        }
+        return null;
     }
 
 
+
+    public Double getValorTotal(){
+        Double valorTotal = 0.0;
+        for (Produto produto : this.produtos) {
+            valorTotal += produto.getPreco();
+        }
+        return valorTotal;
+    }
 }
--- exercicio-relacionamento-main/src/main/java/school/sptech/ex02/Aluno.java
+++ exercicio-relacionamento-main/src/main/java/school/sptech/ex02/Aluno.java
@@ -1,5 +1,57 @@
 package school.sptech.ex02;
 
 public class Aluno {
+    private String nome;
+    private Integer idade;
+    private String matricula;
+    private Double novaProva;
+    private Double notaAtividade;
 
+
+
+    public Double calcularNotaFinal(){
+        Double notaFinal = 0.0;
+        
+        return notaFinal;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public Double getNovaProva() {
+        return novaProva;
+    }
+
+    public Double getNotaAtividade() {
+        return notaAtividade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setIdade(Integer idade) {
+        this.idade = idade;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public void setNovaProva(Double novaProva) {
+        this.novaProva = novaProva;
+    }
+
+    public void setNotaAtividade(Double notaAtividade) {
+        this.notaAtividade = notaAtividade;
+    }
 }
