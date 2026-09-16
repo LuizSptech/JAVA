e920194cfedf5df525e3d122d7498e8cd9f25f09
LuizSptech <luiz.correia@sptech.school>
2026-09-16 12:05:36 -0300

    JDBC - Atividade

---
+++ exercicio-dao-main/src/main/java/school/sptech/Main.java
@@ -0,0 +1,9 @@
+package school.sptech;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Main {
+
+  public static void main(String[] args) {
+  }
+}
---
+++ exercicio-dao-main/src/main/java/school/sptech/MusicaDao.java
@@ -0,0 +1,91 @@
+package school.sptech;
+
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+
+public class MusicaDao {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public MusicaDao(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /* Escreva os métodos abaixo */
+
+        public List<Musica> findAll(){
+                List<Musica> musica = jdbcTemplate.query(
+                        "SELECT * FROM musica;",
+                        new BeanPropertyRowMapper<>(Musica.class));
+                        return musica;
+        }
+
+        public Musica findById(Integer id){
+
+
+               List<Musica> musica = jdbcTemplate.query(
+                       "SELECT * from musica where id = ?",
+                       new BeanPropertyRowMapper<>(Musica.class),id);
+
+
+
+        return musica.isEmpty() ? null : musica.get(0);
+        }
+
+
+        public List<Musica> findByNomeLike(String nome){
+            String name = nome.toLowerCase();
+            List<Musica> musicas = jdbcTemplate.query(
+                    "SELECT * FROM musica WHERE nome ILIKE CONCAT('%', ? ,'%')",
+                    new BeanPropertyRowMapper<>(Musica.class),name);
+
+            return musicas;
+
+        }
+
+
+        public List<Musica> findByArtista(String artista){
+            List<Musica> musicas = jdbcTemplate.query(
+                    "SELECT * FROM musica WHERE artista = ? ",
+                    new BeanPropertyRowMapper<>(Musica.class),artista);
+            return musicas;
+        }
+
+
+        public List<Musica> findByAlbum(String album){
+            List<Musica> musicas = jdbcTemplate.query(
+                    "SELECT * FROM musica WHERE album = ?",
+                    new BeanPropertyRowMapper<>(Musica.class),album);
+            return musicas;
+        }
+
+
+        public List<Musica> findByDuracaoGreaterThan(Integer duracao){
+            List<Musica> musicas = jdbcTemplate.query(
+                    "SELECT * FROM musica WHERE duracao > ?",
+                    new BeanPropertyRowMapper<>(Musica.class),duracao
+            );
+            return musicas;
+        }
+
+        public List<Musica> findByAlbumAndNomeLike(String album, String nome){
+            List<Musica> musicas = jdbcTemplate.query(
+                    "SELECT * FROM musica where album = ? and nome ILIKE  CONCAT('%', ?, '%')",
+                    new BeanPropertyRowMapper<>(Musica.class),album, nome
+            );
+            return musicas;
+        }
+
+        public void save(Musica musica){
+            if (musica.getId() == null){
+            jdbcTemplate.update(
+                    "INSERT INTO musica (nome,artista,album,duracao) values (?,?,?,?)",
+                        musica.getNome(),musica.getArtista(),musica.getAlbum(),musica.getDuracao());
+            }
+            jdbcTemplate.update(
+                    "UPDATE iINTO SET nome = ?, artista"
+            )
+        }
+}
